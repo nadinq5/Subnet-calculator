d0a791b94418dd9a38157b40efd6974d2adb5986
nadinq5 <122729426+nadinq5@users.noreply.github.com>
2023-09-14 10:51:50 +0300

    Update validate_subnet.py

--- validate_subnet.py
+++ validate_subnet.py
@@ -1,11 +1,18 @@
 import re
 
 def validate_subnet(subnet_mask):
-    # Check if the input is empty (indicating it's optional)
+    # Check if the input is empty
     if not subnet_mask:
         return True
 
-    # Check if the input is a valid CIDR notation (e.g., /1 to /32)
-    subnet_pattern = re.compile(r'^\/(1\d|[1-9]|[12]\d|32)$')
+    # Check if the input is a valid CIDR notation (between 1-32)
+    subnet_pattern = re.compile(r'^(1\d|[1-9]|[12]\d|32)$')
     return bool(subnet_pattern.match(subnet_mask))
 
+############# main ###############
+while True:
+    cidr_input = input("Enter CIDR notation (1-32) or leave empty: ")
+    if validate_subnet(cidr_input):
+        break  
+    else:
+        print("Invalid CIDR notation. Must be between 1-32. Please try again.")
