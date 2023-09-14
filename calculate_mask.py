286d79cc033dcaff66571fe9a293681ec36e0396
sawserlauz48 <sawserlauz48@gmail.com>
2023-09-14 11:47:30 +0300

    calculate_mask

--- calculate_mask.py
+++ calculate_mask.py
@@ -6,9 +6,9 @@ def calculate_mask(ip, CIDR):
     binary_mask = "1" * CIDR + "0" * (32 - CIDR)
 
     ### convert the binary subnet mask to decimal
-    subnet_mask_decimal = ".".join(
+    subnet_mask = ".".join(
         str(int(binary_mask[i:i + 8], 2))
         for i in range(0, 32, 8)
     )
 
-    return subnet_mask_decimal
+    return subnet_mask
