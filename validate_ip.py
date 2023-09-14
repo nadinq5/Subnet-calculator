20c0550254c21b816a1ee30573e7d19b25c5b75a
nadinq5 <122729426+nadinq5@users.noreply.github.com>
2023-09-14 11:09:59 +0300

    Create validate_ip.py

---
+++ validate_ip.py
@@ -0,0 +1,14 @@
+import re
+
+def validate_ip(ip):
+    while True:
+        ip_pattern = re.compile(r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')
+        if ip_pattern.match(ip):
+            return ip
+        print("Invalid IP address format. Please enter a valid IP(X.X.X.X), X is a number between 0-255.")
+        ip = input("Enter IP Address: ")
+
+
+############### Main ################
+ip_address = input("Enter IP Address: ")
+valid_ip = validate_ip(ip_address)
