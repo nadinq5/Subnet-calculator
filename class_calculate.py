9b790611970cec6358acccda3b97492a4d0b80dd
ShenhavHershko <141144625+ShenhavHershko@users.noreply.github.com>
2023-09-14 10:33:56 +0300

    Update class_calculate.py
    Update class_calculate.py with interctive user prom

--- class_calculate.py
+++ class_calculate.py
@@ -1,16 +1,16 @@
 def class_calculator(ip):
     first_octet = int(ip.split('.')[0])
     if 1 <= first_octet <= 126:
-        return 'A'
+        return 'The IP address {ip} belongs to Class A'
     elif 128 <= first_octet <= 191:
-        return 'B'
+        return 'The IP address {ip} belongs to Class B'
     elif 192 <= first_octet <= 223:
-        return 'C'
+        return 'The IP address {ip} belongs to Class C'
     elif 224 <= first_octet <= 239:
-        return 'D'
+        return 'The IP address {ip} belongs to Class D'
     elif 240 <= first_octet <= 255:
-        return 'E'
+        return 'The IP address {ip} belongs to Class E'
     elif first_octet == 127:
-        return 'Loopback'
+        return 'The IP address {ip} is Loopback'
     else:
         return None
