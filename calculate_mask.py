e33bf5dfeea0284ec61ee7f365ccbe75f2490bd8
sawserlauz48 <121873964+sawserlauz48@users.noreply.github.com>
2023-09-14 11:57:14 +0300

    Add files via upload

--- calculate_mask.py
+++ calculate_mask.py
@@ -3,7 +3,7 @@
 def calculate_mask(ip, CIDR):
     # convert the subnet mask as a binary string
     binary_mask = "1" * CIDR + "0" * (32 - CIDR)
-    # convert the binary subnet mask to decimal
+    # this for loop go over the binary_mask and convert every octat to int and covert then in base2 to decimal
     subnet_mask_decimal = ".".join(
         str(int(binary_mask[i:i + 8], 2))
         for i in range(0, 32, 8)
@@ -18,7 +18,7 @@ def calculate_mask(ip, CIDR):
     return calced_mask
 
 ip = "192.168.1.1"
-CIDR = 25  # Represents /24 subnet
+CIDR = 25
 subnet_mask = calculate_mask(ip, CIDR)
 print(subnet_mask)
 
