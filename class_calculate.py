87b58edad282efdb85e44c24fcc326222de22435
ShenhavHershko <141144625+ShenhavHershko@users.noreply.github.com>
2023-09-14 10:10:19 +0300

    Add files via upload
    added class_calculate code function

---
+++ class_calculate.py
@@ -0,0 +1,16 @@
+def class_calculator(ip):
+    first_octet = int(ip.split('.')[0])
+    if 1 <= first_octet <= 126:
+        return 'A'
+    elif 128 <= first_octet <= 191:
+        return 'B'
+    elif 192 <= first_octet <= 223:
+        return 'C'
+    elif 224 <= first_octet <= 239:
+        return 'D'
+    elif 240 <= first_octet <= 255:
+        return 'E'
+    elif first_octet == 127:
+        return 'Loopback'
+    else:
+        return None
