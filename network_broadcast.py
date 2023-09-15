75cf38af6aad9b01f428319e6c78efaf94916038
c00lgat <a28_a3@yahoo.com>
2023-09-15 12:59:15 +0300

    Added comments

--- network_broadcast.py
+++ network_broadcast.py
@@ -6,47 +6,63 @@ from itertools import chain
 subnet_mask = "255.128.0.0"
 ip_address = "39.229.177.100"
 
+# These return a list containing the octets, with each octet being stored in a different index altogether in a list
 subnet_list = subnet_mask.split('.')
 ip_list = ip_address.split('.')
 
+# Initializing lists
 bin_subnet_list = []
 bin_ip_address_list = []
 network_id_chain = []
 broadcast_id_chain = []
 
 
-
+# Filled in the necessary zeroes while also converting the subnet mask and the ip address to binary
 for item in subnet_list:
     item = (bin(int(item))[2:]).zfill(8)
     bin_subnet_list.append(item)
-
+# Filled in the necessary zeroes while also converting the subnet mask and the ip address to binary
 for ip in ip_list:
     ip = (bin(int(ip))[2:]).zfill(8)
     bin_ip_address_list.append(ip)
 
-
+# Converted each list to chains, so that it would be easier
+# doing the bitwise calculations as we iterate over each bit at a time
 subnet_chain = list(chain.from_iterable(bin_subnet_list))
 ip_chain = list(chain.from_iterable(bin_ip_address_list))
 
+# The bitwise calculations between the subnet mask and the ip address.
+# we iterate over each of the two in their binary form and do the bitwise
+# calculations needed.
 for i in range(len(subnet_chain)):
     network_id_chain.append(int((int(subnet_chain[i]) and int(ip_chain[i]))))
     broadcast_id_chain.append(int((int(ip_chain[i]) or (not(int(subnet_chain[i]))))))
 
-
+# Converts the chain back into a string form and then we iterate over the string
+# 8 characters at a time in order to capture the octets. And then we
+# concatenate them using the join() function.
+# bin_network_id is the binary form of the network id after the calculations
+# in a string form with dots separating the octets
 binary_string = "".join(str(network_id_chain[i]) for i in range(len(network_id_chain)))
 octet = [binary_string[i:i+8] for i in range(0, 32, 8)]
 bin_network_id = '.'.join(octet)
-
+# Converts the chain back into a string form and then we iterate over the string
+# 8 characters at a time in order to capture the octets. And then we
+# concatenate them using the join() function.
+# bin_network_id is the binary form of the network id after the calculations
+# in a string form with dots separating the octets
 binary_string = "".join(str(broadcast_id_chain[j]) for j in range(len(broadcast_id_chain)))
 octet = [binary_string[i:i+8] for i in range(0, 32, 8)]
 bin_broadcast_id = '.'.join(octet)
 
-
+# Converts the network id and broadcast id that were calculated above into decimal
+# notation.
 network_id = ".".join(str(int((bin_network_id[i:i+8]),2)) for i in range(0, 35, 9))
 broadcast_id = ".".join(str(int((bin_broadcast_id[i:i+8]),2)) for i in range(0, 35, 9))
 
 print(network_id)
 print(broadcast_id)
+
 
 if(int(network_id.split(".")[-1]) < 255):
     first_host = network_id.split(".")[:3]+([str(int(network_id.split(".")[-1]) + 1)])
@@ -59,6 +75,7 @@ elif(int(network_id.split(".")[-1]) == 255):
         first_host = network_id.split(".")[:1] + ([str(int(network_id.split(".")[-3]) + 1)])
         first_host = ".".join(str(first_host[j]) for j in range(len(first_host)))
 
+# Calculates the last host. Takes the last octet in the broadcast and subtracts one from it
 last_host = broadcast_id.split(".")[:3] + [str(int((broadcast_id.split(".")[-1])) - 1)]
 last_host = ".".join(str(last_host[j]) for j in range(len(last_host)))
 
